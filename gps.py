1efc9f1389ab6cfb88d9039000bef4b1e48eae1a
root <root@raspberrypi.(none)>
2015-02-21 14:22:06 +0900

    modified gps.py

--- gps.py
+++ gps.py
@@ -1,5 +1,19 @@
+import sys
 import serial
+import datetime
 
+start_time = datetime.datetime.now()
+print start_time
+f = open('gps_log.txt','w')
 ser = serial.Serial('/dev/ttyAMA0',4800)
 while 1:
-	print ser.readline()
+        process_time = datetime.datetime.now()
+        dt =  process_time - start_time
+        result = dt.seconds
+	if result >= 10:
+	        sys.exit()
+        else:
+                print ser.readline()
+                f.write(ser.readline())
+
+
